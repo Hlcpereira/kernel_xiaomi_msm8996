41433c01948230c219d3d14f7996832cf1a0b6e6
brigio345 <brigio345@gmail.com>
2018-03-24 14:48:13 +0100

    treewide: fix -Wtautological-constant-out-of-range-compare clang 7.0 warnings
    Signed-off-by: brigio345 <brigio345@gmail.com>

--- drivers/leds/leds-qpnp.c
+++ drivers/leds/leds-qpnp.c
@@ -1990,8 +1990,7 @@ static int qpnp_wled_init(struct qpnp_led_data *led)
 		return -EINVAL;
 	}
 
-	if ((led->wled_cfg->ctrl_delay_us % WLED_CTL_DLY_STEP) ||
-		(led->wled_cfg->ctrl_delay_us > WLED_CTL_DLY_MAX)) {
+	if (led->wled_cfg->ctrl_delay_us % WLED_CTL_DLY_STEP) {
 		dev_err(&led->spmi_dev->dev, "Invalid control delay\n");
 		return -EINVAL;
 	}
@@ -3721,7 +3720,7 @@ static int qpnp_get_config_kpdbl(struct qpnp_led_data *led,
 	rc = of_property_read_string(node, "qcom,mode", &mode);
 	if (!rc) {
 		led_mode = qpnp_led_get_mode(mode);
-		if ((led_mode == MANUAL_MODE) || (led_mode == -EINVAL)) {
+		if (led_mode == MANUAL_MODE) {
 			dev_err(&led->spmi_dev->dev, "Selected mode not " \
 				"supported for kpdbl.\n");
 			return -EINVAL;
@@ -3787,7 +3786,7 @@ static int qpnp_get_config_rgb(struct qpnp_led_data *led,
 	rc = of_property_read_string(node, "qcom,mode", &mode);
 	if (!rc) {
 		led_mode = qpnp_led_get_mode(mode);
-		if ((led_mode == MANUAL_MODE) || (led_mode == -EINVAL)) {
+		if (led_mode == MANUAL_MODE) {
 			dev_err(&led->spmi_dev->dev, "Selected mode not " \
 				"supported for rgb.\n");
 			return -EINVAL;
@@ -3914,12 +3913,6 @@ static int qpnp_get_config_mpp(struct qpnp_led_data *led,
 		led->mpp_cfg->pwm_mode = led_mode;
 		if (led_mode == MANUAL_MODE)
 			return MANUAL_MODE;
-		else if (led_mode == -EINVAL) {
-			dev_err(&led->spmi_dev->dev, "Selected mode not " \
-				"supported for mpp.\n");
-			rc = -EINVAL;
-			goto err_config_mpp;
-		}
 		led->mpp_cfg->pwm_cfg = devm_kzalloc(&led->spmi_dev->dev,
 					sizeof(struct pwm_config_data),
 					GFP_KERNEL);
